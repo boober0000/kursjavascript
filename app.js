53d4673a648dc17bdc98dcbf5562a8777a3b53e2
boober0000 <131643036+boober0000@users.noreply.github.com>
2023-05-03 14:12:31 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
-let name = "dupa";
-let lastName = 'dupowicz'
+let activeSlideNumber = 1;
 
-console.log(name, 1, 1, 2+2);
-console.log(2/2, name + ' '+ lastName);
+let arrowLeft = document.querySelector('.arrow-left')
+let arrowRight = document.querySelector('.arrow-right')
 
+
+let hideActiveSlide = () => {
+    let activeElement = document.querySelector('.active');
+    activeElement.classList.remove('active');
+}
+
+let showSlide = (slideNumber) => {
+    hideActiveSlide();
+    document.querySelector('#slide'+slideNumber).classList.add('active');
+}
+
+let showNextSlide = () => {
+    if(activeSlideNumber === 3){
+        activeSlideNumber = 1;
+    } else {
+        activeSlideNumber = activeSlideNumber +1;
+    }
+    showSlide(activeSlideNumber);
+};
+
+let showPreviousSlide = () => {
+    if(activeSlideNumber === 1) {
+        activeSlideNumber = 3;
+    } else {
+        activeSlideNumber = activeSlideNumber -1;
+    }
+    showSlide(activeSlideNumber);
+}
+
+
+  for(let i = 1; i <= 3; i++) {
+    let showSlideI = () => {
+        activeSlideNumber = i;
+        showSlide(i);
+    };
+    document.querySelector('#dot'+i).addEventListener('click', showSlideI);
+   }
+
+
+
+
+arrowRight.addEventListener('click', showNextSlide);
+arrowLeft.addEventListener('click', showPreviousSlide);
